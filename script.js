9e1ecec7dd6fac0de6f119fce8c7f8d73adbbfed
Erik Bejstam <erikbejstam@hotmail.com>
2025-02-01 16:45:15 +0100

    Remove script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let newDiv = document.createElement("div"); //Skapa ett element som vi kallar newDiv. document kommer att länka med docet som detta script åkallas ifrån.
-newDiv.innerHTML = "This is a dynamically created div!"; //Sätt själva texten i div:en.
-newDiv.classList.add("newDiv");
-document.body.appendChild(newDiv);
