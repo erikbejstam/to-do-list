6a7c11ef03cae4b9554d64e3068647ded3ac1c12
Erik Bejstam <erikbejstam@hotmail.com>
2025-02-01 16:28:52 +0100

    Convert JS to TS

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
+"use strict";
 let newDiv = document.createElement("div"); //Skapa ett element som vi kallar newDiv. document kommer att länka med docet som detta script åkallas ifrån.
-
-newDiv.innerHTML = "This is a dynamically created div!" //Sätt själva texten i div:en.
-
-newDiv.classList.add("newDiv")
-
-document.body.appendChild(newDiv)
-
+newDiv.innerHTML = "This is a dynamically created div!"; //Sätt själva texten i div:en.
+newDiv.classList.add("newDiv");
+document.body.appendChild(newDiv);
