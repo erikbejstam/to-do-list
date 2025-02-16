5e232dc7e487c9f0a2f9f436e654506ec886f69b
Erik Bejstam <erikbejstam@hotmail.com>
2025-02-16 15:02:54 +0100

    Add MutationObserver so that fade-out appears only when the scrollbar appears.

--- src/tasklist.ts
+++ src/tasklist.ts
@@ -2,12 +2,43 @@ class TaskList {
     private tasks: Map<number, Task>;
     private element: HTMLUListElement;
     public renderCallback: () => void;
+    private observer: MutationObserver;
 
     constructor() {
         this.tasks = new Map<number, Task>();
         this.element = document.querySelector(".task-list") as HTMLUListElement;
         this.renderCallback = () => {};
         this.render();
+        this.observer = this.initObserver();
+        this.element.addEventListener('scroll', () => this.updateMask());
+    }
+
+    initObserver(): MutationObserver {
+        const observer = new MutationObserver(() => this.handleMutation())
+        observer.observe(this.element, { childList: true, subtree: true });
+        return observer
+    }
+
+    handleMutation(): void {
+        if (this.element.scrollHeight > this.element.clientHeight) {
+            this.updateMask();
+        }
+    }
+
+    updateMask(): void {
+        const scrollTop = this.element.scrollTop;
+        const clientHeight = this.element.clientHeight;
+        const scrollHeight = this.element.scrollHeight;
+        const atTop = scrollTop === 0;
+        const atBottom = scrollTop + clientHeight >= scrollHeight;
+
+        if (atTop) {
+            this.element.style.maskImage = "linear-gradient(to bottom, black 90%, transparent)";
+        } else if (atBottom) {
+            this.element.style.maskImage = "linear-gradient(to top, black 90%, transparent)";
+        } else {
+            this.element.style.maskImage = "linear-gradient(to bottom, transparent, black 20%, black 80%, transparent)";
+        }
     }
 
     render() {
@@ -32,7 +63,6 @@ class TaskList {
 
     deleteTask(id: number): void {
         const task = this.tasks.get(id);
-        console.log(this.tasks)
         if (task) {
             task.element.remove();
             this.tasks.delete(id);
