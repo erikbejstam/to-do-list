966dc318d62393ccb30260ed06aa336419b2e3e6
Erik Bejstam <erikbejstam@hotmail.com>
2025-05-23 15:07:08 +0200

    Add create controller to server.ts

--- src/server.ts
+++ src/server.ts
@@ -9,10 +9,26 @@ app.use(express.json()); // allows us to read JSON from request bodies
 // Routes will go here
 
 app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+    console.log('Server running at http://localhost:3000');
 });
 
 app.get('/tasks', async (req, res) => {
     const tasks = await prisma.task.findMany();
     res.json(tasks);
+});
+
+app.put('/tasks/:id', async (req, res) => {
+    const id = Number(req.params.id);
+    const { text } = req.body;
+
+    try {
+        const task = await prisma.task.update({
+            where: { id },
+            data: { text },
+        });
+
+        res.json(task);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update task' });
+    }
 });
